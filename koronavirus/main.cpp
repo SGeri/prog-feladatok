b851d8542a7cfa9973f9ef3d91356ad6af11924b
Sárffy Gergő <sarffygeri@gmail.com>
2022-10-23 23:00:06 +0200

    koronavirus wip

--- koronavirus/main.cpp
+++ koronavirus/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// wip todo:
+// ha egy szekció megvan, akkor az összes eleme skipelhető
+// a be1-nél valami el van baszva, a 2-es indexnél 4-en ír valamiért (valszeg a végével nem jó valami)
+
 int main()
 {
-    int max;
-    cin >> max;
-
+    // read data
     int values_length;
     cin >> values_length;
+
+    int max;
+    cin >> max;
 
     int values[values_length];
     for (int i = 0; i < values_length; i++)
@@ -15,42 +20,44 @@ int main()
         cin >> values[i];
     }
 
-    int max_infection = 0;
-    int min_infection = 0;
+    // declare logic variables
+    int max_infection = -1;
+    int min_infection = -1;
 
-    for (int i = 0; i < values_length; i++)
+    // loop through values
+    // to find the max and min infection amount
+    for (int starter_index = 0; starter_index < values_length; starter_index++)
     {
-        int starter_index = i;
+        int local_infection = 1;
 
-        int local_max_infection = 0;
-        int local_min_infection = 0;
-
-        for (int i = 0; i < values_length; i++)
+        // only have to go in the right direction
+        // because the left direction is already checked by the fact that
+        // there are sections where the infection amount is the same
+        for (int i = 0; i < values_length - starter_index; i++)
         {
-            int left_offset_index = starter_index - i;
-            int right_offset_index = starter_index + i;
-
-            if (left_offset_index < 0)
-                continue;
-            if (right_offset_index > values_length - 1)
-                continue;
-
-            if (values[left_offset_index] <= values[left_offset_index + 1] - max)
+            if (values[starter_index + i] + max >= values[starter_index + i + 1])
             {
-                local_max_infection++;
+                local_infection++;
             }
-
-            if (values[right_offset_index] <= values[right_offset_index - 1] - max)
+            else
             {
-                local_min_infection++;
+                break;
             }
         }
 
-        if (local_max_infection > max_infection)
-            max_infection = local_max_infection;
+        // compare local infection
+        if (local_infection > max_infection || max_infection == -1)
+        {
+            max_infection = local_infection;
+        }
 
-        if (local_min_infection > min_infection)
-            min_infection = local_min_infection;
+        if (local_infection < min_infection || min_infection == -1)
+        {
+            min_infection = local_infection;
+        }
+
+        // for testing purposes
+        // cout << "starter_index: " << starter_index << " starter_value: " << values[starter_index] << " local_infection: " << local_infection << endl;
     }
 
     cout << max_infection << " " << min_infection << endl;
